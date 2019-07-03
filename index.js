f6f60aed4134664a1affa7ce6ccf2c637cfbae10
caiohenriquesk <caiohenriquesk@gmail.com>
2019-07-02 23:22:14 -0300

    Criação das rotas

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const server = express();
 
-server.get("", (req, res) => {
-  return console.log("foi");
+const projetos = ["teste 1", "teste 2", "teste 3"];
+
+server.post("/projects", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.params;
+  console.log(id);
+  console.log(title);
+  return res.json(projetos);
+});
+
+server.get("/projects", (req, res) => {
+  return res.json(projetos);
+});
+
+server.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  return null;
+});
+
+server.delete("/projects/:id", (req, res) => {
+  return null;
+});
+
+server.post("/projects/:id/tasks", (res, req) => {
+  return null;
 });
 
 server.listen(3000);
