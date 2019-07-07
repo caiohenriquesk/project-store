9454bebcc5310331a918a4ed59fb5019247fd444
caiohenriquesk <caiohenriquesk@gmail.com>
2019-07-07 19:51:27 -0300

    resolução final

--- index.js
+++ index.js
@@ -4,12 +4,37 @@ const server = express();
 server.use(express.json());
 
 const projetos = [];
+var callCount = 0;
+
+//middleware's
+server.use((req, res, next) => {
+  next();
+  callCount++;
+  console.log("Foi chamado " + callCount + " vezes.");
+});
+
+function checkProjeto(req, res, next) {
+  const { id } = req.params;
+  var have = false;
+
+  for (projeto of projetos) {
+    if (projeto.id == id) {
+      have = true;
+    }
+  }
+
+  if (!have) {
+    return res.status(400).json({ error: "There isnt this id" });
+  }
+
+  return next();
+}
 
 server.post("/projects", (req, res) => {
   const { id } = req.body;
   const { title } = req.body;
 
-  var projeto = `{ id: ${id}, title: ${title}, task: [] }`;
+  var projeto = { id: id, title: title, tasks: [] };
   projetos.push(projeto);
 
   return res.json(projetos);
@@ -19,30 +44,49 @@ server.get("/projects", (req, res) => {
   return res.json(projetos);
 });
 
-server.put("/projects/:id", (req, res) => {
+server.put("/projects/:id", checkProjeto, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
-  var projeto = `{ id: ${id}, title: ${title}, task: [] }`;
-  projetos[id] = projeto;
+  for (projeto of projetos) {
+    if (projeto.id == id) {
+      projeto.title = title;
+    }
+  }
 
   return res.json(projetos);
 });
 
-server.delete("/projects/:id", (req, res) => {
+server.delete("/projects/:id", checkProjeto, (req, res) => {
   const { id } = req.params;
-  projetos.splice(id, 1);
+
+  var count = 0;
+  var positionToDelete = [];
+  for (projeto of projetos) {
+    if (projeto.id == id) {
+      positionToDelete.push(count);
+    }
+    count++;
+  }
+
+  if (positionToDelete !== []) {
+    for (obj of positionToDelete) {
+      projetos.splice(obj);
+    }
+  }
 
   return res.json(projetos);
 });
 
-server.post("/projects/:id/tasks", (req, res) => {
+server.post("/projects/:id/tasks", checkProjeto, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  const { task } = req.body;
 
-  var projeto = `{ id: ${id}, title: ${title}, task: [${task}] }`;
-  projetos[id] = projeto;
+  for (projeto of projetos) {
+    if (projeto.id == id) {
+      projeto.tasks.push(title);
+    }
+  }
 
   return res.json(projetos);
 });
